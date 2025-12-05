918ec59ed688105c7d651fdedc86ed6708a98781
Rafael Almeida <rafaeltesterqa@gmail.com>
2025-12-05 08:29:45 -0300

    Testando campos select

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -103,4 +103,21 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   });
 
+  it.only('seleciona um produto (youtube) por seu texto', () => {
+    cy.get('#product').select('YouTube')
+      .should('have.value', 'youtube')
+  });
+
+  it.only('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product').select('mentoria')
+      .should('have.value', 'mentoria')
+  });
+
+  it.only('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1)
+      .should('have.value', 'blog')
+  });
+
+
+
 })
